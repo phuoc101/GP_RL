0f472b7893e64dd76cd74c7b939863260e6a9323
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-11-16 18:06:10 +0200

    fixed example_GP after feedback

--- code_base/gp-learning/GaussianProcess/example_GP.py
+++ code_base/gp-learning/GaussianProcess/example_GP.py
@@ -7,6 +7,11 @@ from loguru import logger
 from cfg import configs
 from models.GPModel import GPModel
 from utils.data_loading import load_test_data
+import os
+from pathlib import Path
+
+# * make the paths absolute instead of relative, to avoid headaches
+dir_path = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main(opts):
@@ -18,28 +23,37 @@ def main(opts):
         path_train_data=opts.train,
     )
     pred_mean, pred_conf = model.eval(path_test_data=opts.test)
+    # X_test is (pos, vel, u)
     X_test, y_test = load_test_data(data_path=opts.test)
     X_test = X_test.numpy()
     real_pos = X_test[:, 0]
-    real_vel = X_test[:, 1]
+    # real_vel = X_test[:, 1]
+    # * GP predictions should be compared with labels (y), not inputs (X) because the
+    # inputs skip certain processings like normalization and scalings
+    real_dx = y_test[:, 0]  # label of dx
+    real_dv = y_test[:, 1]
     pos_init = X_test[0, 0]
-    vel_init = X_test[0, 1]
     pos_pred_mean_delta = pred_mean[:, 0]
-    vel_pred_mean_delta = pred_mean[:, 1]
     pos_pred_mean = np.ones(len(pos_pred_mean_delta)) * pos_init
-    vel_pred_mean = np.ones(len(pos_pred_mean_delta)) * vel_init
+    # this is dead reckoning, it's not going to be a useful as an evaluation tool if
+    # it's done for long horizons because it is open-loop and model errors will grow.
     for i, delta in enumerate(pos_pred_mean_delta):
         if i > 0:
             pos_pred_mean[i] = pos_pred_mean[i - 1] + delta
-    for i, delta in enumerate(vel_pred_mean_delta):
-        if i > 0:
-            vel_pred_mean[i] = vel_pred_mean[i - 1] + delta
-    plt.title(f"GP model prediction on {opts.test}")
-    plt.plot(real_pos, label="real_pos")
-    plt.plot(pos_pred_mean, label="pred_pos")
-    plt.plot(real_vel, label="real_vel")
-    plt.plot(vel_pred_mean, label="pred_vel")
-    plt.legend()
+    fig, ax = plt.subplots(3, 1)
+    ax[0].set_title(f"GP model prediction on {opts.test}")
+    ax[0].plot(real_pos, label="real_pos")
+    ax[0].plot(pos_pred_mean, label="integrated_pos")
+    ax[1].plot(real_dx, label="real_dx")
+    ax[1].plot(pred_mean[:, 0], label="pred_dx")
+    # ax[1].plot(real_vel, label="real velocity")
+    ax[2].plot(real_dv, label="real_dv")
+    ax[2].plot(pred_mean[:, 1], label="pred_dv")
+
+    ax[0].legend()
+    ax[1].legend()
+    ax[2].legend()
+
     plt.show()
 
 
@@ -48,25 +62,23 @@ if __name__ == "__main__":
     logger.add(sys.stderr, level="DEBUG")
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-t",
         "--train",
         type=str,
-        default="../data/avant_TrainingData.pkl",
+        default=dir_path / "../data/avant_TrainingData.pkl",
         help="Path to training data",
-    )
-    parser.add_argument(
-        "-T",
-        "--test",
-        type=str,
-        default="../data/avant_TestData.pkl",
-        help="Path to test data",
     )
     parser.add_argument(
         "-gp",
         "--gpmodel",
         type=str,
-        default="./results/GPmodel.pkl",
+        default=dir_path / "./results/GPmodel.pkl",
         help="Path to training data",
+    )
+    parser.add_argument(
+        "--test",
+        type=str,
+        default=dir_path / "../data/avant_TestData.pkl",
+        help="Path to test data",
     )
     parser.add_argument(
         "--gp-train-iter",

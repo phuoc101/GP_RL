150b96fb556c900562e51cf29051e3d81f071432
Rufat Bayramov <rufat.rbayramov@gmail.com>
2022-11-21 01:04:54 +0300

    proper boom position/velocity and input reading

--- code_base/gp-learning/GaussianProcess/process_bag_data.py
+++ code_base/gp-learning/GaussianProcess/process_bag_data.py
@@ -42,8 +42,18 @@ type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_ty
 
 
 boom_start_nanosec = 0.0
-boom_array = []
+boom_input_start_nanosec = 0.0
+
+boom_position_array = []
+boom_velocity_array = []
 boom_time_array = []
+
+
+boom_position_delta_array = []
+boom_velocity_delta_tarray = []
+
+boom_input_array = []
+boom_input_time_array = []
 
 
 
@@ -53,16 +63,37 @@ while reader.has_next():
         msg_type = get_message(type_map[topic])
         msg = deserialize_message(data, msg_type)
 
-    if topic == "/boom_inclinometer/filtered":
-        if len(boom_array)==0:
+
+    if topic == "/bag_joint_states":
+        if len(boom_time_array)==0:
             boom_start_nanosec = t 
         diff = (t - boom_start_nanosec)/10e8
         print("| t = " + str(diff)+" seconds |")
-        boom_array.append(msg.position[0])
+
+        boom_position_array.append(msg.position[2])
+        boom_velocity_array.append(msg.velocity[2])
         boom_time_array.append(diff)
 
+    if topic == "/input_valve_cmd":
+        if len(boom_input_time_array)==0:
+            boom_input_start_nanosec = t 
+        diff = (t - boom_start_nanosec)/10e8
+        print("| t = " + str(diff)+" seconds |")
+        if (msg.position[1]>100):
+            boom_input_array.append(-(msg.position[1]-150)/100)
+        else:
+            boom_input_array.append(msg.position[1]/100)
+
+        boom_input_time_array.append(diff)
+
+print("position/velocity length: " + str(len(boom_position_array)))
+print("input length: " + str(len(boom_input_array)))
+
+plt.plot(boom_time_array,boom_position_array)
+plt.plot(boom_time_array,boom_velocity_array)
+plt.plot(boom_input_time_array,boom_input_array)
+
+#TODO downsampling and state difference
 
 
-
-plt.plot(boom_time_array,boom_array)
 plt.show()
